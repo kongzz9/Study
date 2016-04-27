f7b9e1d07c1102e40bf5f893efcf3079d3d6e46c
kongzz <kongzz@kimeye.com>
2016-04-27 11:37:11 +0900

    C# Sample Code

---
+++ CSharpTest/WatcherConsole/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace WatcherConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileSystemWatcher incoming = new FileSystemWatcher();
+
+            incoming.Path = @"C:\DreamSoft";
+            incoming.NotifyFilter = NotifyFilters.LastAccess |
+                                    NotifyFilters.LastWrite |
+                                    NotifyFilters.FileName |
+                                    NotifyFilters.DirectoryName;
+            incoming.Filter = "*.*";
+
+            incoming.Changed += Incoming_Changed;
+
+            incoming.EnableRaisingEvents = true;
+
+            Console.WriteLine("Press \'q\' to quit.");
+            while (Console.Read() != 'q') ;
+
+        }
+
+        private static void Incoming_Changed(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine("{0} : Changed", e.FullPath);
+        }
+    }
+}
