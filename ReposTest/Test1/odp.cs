262645a7e3b4daa043fb52bb2576ab76da139545
kongzz <kongzz@kimeye.com>
2016-04-28 09:48:36 +0900

    opd.cs 추가 Oracle 설치 안 된 자리

---
+++ ReposTest/Test1/odp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data;
+
+namespace Test1
+{
+    public class odp : IDisposable       
+    {
+        public odp()
+        {
+
+        }
+
+
+        public void Dispose()
+        {
+            
+        }
+    }
+}
